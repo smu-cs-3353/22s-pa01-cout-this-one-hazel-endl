822d4cd5fb4c042a440ea1e2670953e9a2958141
London Kasper <lckasper@smu.edu>
2022-01-24 22:45:05 -0600

    Added src, input folders. Created main class and started CMake

---
+++ cmake-build-debug/CMakeFiles/22s_pa01_cout_this_one_hazel_endl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "22s_pa01_cout_this_one_hazel_endl"
+  "22s_pa01_cout_this_one_hazel_endl.pdb"
+  "CMakeFiles/22s_pa01_cout_this_one_hazel_endl.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_pa01_cout_this_one_hazel_endl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by London Kasper on 1/24/2022.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello world!" << endl;
+}
