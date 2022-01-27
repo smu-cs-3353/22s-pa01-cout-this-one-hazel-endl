64f52e465edebc26fd1bb9ca736c92d06ea19dcd
hazele1017 <heroy@mail.smu.edu>
2022-01-26 21:25:04 -0600

    Created Painting Class

--- src/Painting.cpp
+++ src/Painting.cpp
@@ -1,5 +1,34 @@
 //
 // Created by Hazel Eroy on 1/26/22.
 //
-
+#include <iostream>
+using namespace std;
 #include "Painting.h"
+
+Painting::Painting() {
+     ID = -1;
+     Price = -1.0;
+}
+
+Painting::Painting(int IDnum, double value) {
+     ID = IDnum;
+     Price = value;
+}
+
+int Painting::getID() {
+    return ID;}
+
+void Painting::setID(int IDnum) {
+    ID = IDnum;}
+
+void Painting::setPrice (double value) {
+    Price = value;}
+
+double Painting::getPrice() {
+    return Price;}
+
+void Painting::print(){
+    cout << "ID: " << getID() << endl;
+    cout << "Price: $" << getPrice() <<  endl;
+   // cout << "Dimensions (W x H): " << width << " " << height << endl;
+};
--- src/Painting.h
+++ src/Painting.h
@@ -4,10 +4,26 @@
 
 #ifndef INC_22S_PA01_COUT_THIS_ONE_HAZEL_ENDL_PAINTING_H
 #define INC_22S_PA01_COUT_THIS_ONE_HAZEL_ENDL_PAINTING_H
-
-
+#include <iostream>
+using namespace std;
 class Painting {
 
+    public:
+    Painting();
+
+    Painting  (int IDnum, double value);
+
+        int getID();
+        void setID (int num);
+
+        void setPrice (double value);
+        double getPrice();
+
+        void print();
+
+    private:
+        int ID;
+        double Price;
 };
 
 
