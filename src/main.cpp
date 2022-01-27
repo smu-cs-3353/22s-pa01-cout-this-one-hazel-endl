ddf7155f67634991fb69452401252554ec5eb017
London Kasper <lckasper@smu.edu>
2022-01-26 21:24:38 -0600

    Added command line arguments to access input file, testing input on test.txt

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,48 @@
 // Created by London Kasper on 1/24/2022.
 //
 #include <iostream>
+#include <fstream>
+
 using namespace std;
-int main(){
-    cout << "Hello world!" << endl;
-    cout << "Hi Hazel!" << endl;
+int main(int argc,  char *argv[]){
+    if(argc > 1){
+        cout << argv[1] << endl;
+        char* fileIn = argv[1];
+        //will also need three output files to be declared, each one appended to the end of the input file's name (see project guidelines)
+        fstream inFS(fileIn);
+        if(!inFS.is_open()){
+            cout << "File did not open." << endl;
+        }
+        else {
+            cout << "File opened." << endl;
+        }
+        string maxWidth;
+        char buffer[3000];
+
+        inFS.getline(buffer, 3000, ' ');
+        maxWidth = buffer;
+        cout << maxWidth << endl;
+        inFS.getline(buffer, 3000, '\n');
+        string maxHeight = buffer;
+        cout << maxHeight << endl;
+        inFS.getline(buffer, 3000, '\n');
+        string numPaintings = buffer;
+        cout << numPaintings << endl;
+        inFS.getline(buffer, 3000, ' ');
+        while(!inFS.eof()){
+            string id = buffer;
+            inFS.getline(buffer, 3000, ' ');
+            string price = buffer;
+            inFS.getline(buffer, 3000, ' ');
+            string width = buffer;
+            inFS.getline(buffer, 3000, '\n');
+            string height = buffer;
+            cout << "Painting ID: " << id << "  Price: " << price << "  Width: " << width << endl;
+            inFS.getline(buffer, 3000, ' ');
+        }
 
 
-    cout << "added";
 
-    cout << "hi london";
+
+    }
 }
